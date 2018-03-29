33c6692e6426e4c084cb900274fdb4a265e3448e
kisiyama <kishiyama.t@hotmail.com>
2018-03-29 22:25:43 +0900

    スクリプトにしました。

---
+++ script/data-analyzing.r
@@ -0,0 +1,190 @@
+# data analyzing
+
+1.  if(!require(<name>)){install.packages("<name>")}
+    * it does "library(<name>)." if R doesn't have the package, R installs it.
+1. knitr is awesome. Highly recommended!
+    * [Knitr is Awesome!](https://www.r-bloggers.com/knitr-is-awesome/)
+    * [Markdown table](https://stats.biopapyrus.jp/r/devel/md-table.html)
+1. beepr allows you not to leave while waiting.
+    * [beepr-stackoverflow](https://stackoverflow.com/questions/3365657/is-there-a-way-to-make-r-beep-play-a-sound-at-the-end-of-a-script)
+1. set up (set wd, import libs, and read data)
+
+```R
+getwd()
+setwd("/home/kishiyama/home/thesis/ntu-ut-ling-vwp/result")
+setwd("/home/kisiyama/home/thesis/ntu-ut-ling-vwp/result")
+
+# install package required.
+if(!require(lme4)){install.packages("lme4")}
+# if you're using WSL, you need to have some packages before.
+if(!require(reshape)){install.packages("reshape")}
+if(!require(reshape2)){install.packages("reshape2")}
+if(!require(ggplot2)){install.packages("ggplot2")}
+if(!require(knitr)){install.packages("knitr")}
+if(!require(beepr)){install.packages("beepr")}
+if(!require(lmerTest)){install.packages("lmerTest")}
+# if(!require(magrittr)){install.packages("magrittr")}
+if(!require(devtools)){install.packages("devtools")}
+install_github("kisiyama/mudball",ref="master",force=TRUE)
+require(mudball)
+
+library(lme4)
+library(reshape)
+
+data <- read.csv("./output.csv", header =T)
+head(data)
+# check its distribution
+table(data$ParticipantName, data$Condition)
+table(data$Condition)
+
+# AOIs has numbers . see the content in the AOI.
+# as same as the way we took in the visualization step
+data$target <- ifelse(data$AOI == 1, as.character(data$AOI1), "BackGround")
+data$target <- ifelse(data$AOI == 2, as.character(data$AOI2), data$target)
+data$target <- ifelse(data$AOI == 3, as.character(data$AOI3), data$target)
+data$target <- ifelse(data$AOI == 4, as.character(data$AOI4), data$target)
+
+data <-data[order(data$ParticipantName, data$ItemNo, data$GazeStart),]
+```
+
+1. We want to extract the data in the region we are interested in.
+
+```R
+# define the span for the graph here
+onset_var <- 4700
+offset_var <- 5100
+span <- offset_var - onset_var
+
+# start lapse and end lapse
+data$slapse <- data$GazeStart - onset_var
+data$elapse <- data$GazeEnd - onset_var
+head(data)
+
+# The lapse(slapse) until the onset become negative values
+# we will ignore the negatives later
+# not taking them into account
+
+#  slapse elapse
+#1   -824   -670
+#2   -603   -397
+#3      3    173
+#4    177    680
+#5    926   1523
+#6   1220   1603
+
+data$slapse <- ifelse(data$slapse < 0, 0, data$slapse)
+data$elapse <- ifelse(data$elapse >= span, span, data$elapse)
+
+#  slapse elapse
+#1      0   -199
+#2      0    -10
+#3      3      3
+#4    177    177
+#5    926   1000
+#6   1220   1000
+
+data$dur <- data$elapse - data$slapse
+data$dur <- ifelse(data$dur < 0, 0, data$dur)
+
+# by subtracting slapse from elapse,
+# we can focus on the area with positive value(dur)
+
+data <- data[,c("ParticipantName", "ItemNo", "target", "Condition", "slapse","elapse","dur")]
+data <- data[order(data$ParticipantName,data$ItemNo,data$slapse),]
+
+# CALUCULATING SUM (aggregation for each trial)
+data <-aggregate(
+    data$dur,
+    by=list(data$ParticipantName, data$ItemNo, data$target, data$Condition),
+    FUN=sum,
+    na.rm=TRUE)
+colnames(data) = c("subj","item","AOI","cond","sum")
+
+# sort
+data <- data[order(data$subj, data$item),]
+
+# set colname "variable","value" to use cast()
+colnames(data) = c("subj","item","variable","cond","value")
+head(data)
+# cast creates separate columns for each object fixated
+
+data2 <- cast(data)
+head(data2)
+
+# replace NULL
+data2$BackGround <- ifelse(is.na(data2$BackGround), 0, data2$BackGround)
+data2$A<- ifelse(is.na(data2$A), 0, data2$A)
+data2$B<- ifelse(is.na(data2$B), 0, data2$B)
+data2$C<- ifelse(is.na(data2$C), 0, data2$C)
+data2$D<- ifelse(is.na(data2$D), 0, data2$D)
+
+data2$Target <- ifelse((data2$cond == "a" | data2$cond == "b"), data2$C, data2$D)
+head(data2)
+
+#  head(data2)
+# # subj item cond   A   B BackGround   C D Target all      logit
+# 1  P05    1    a   0 195          0  99 0     99 294 -0.6754027
+# 2  P05    2    d   0 400          0   0 0      0 400 -6.6858609
+# 3  P05    3    c 391   0          0   0 0      0 391 -6.6631327
+# 4  P05    4    b   0   0          0 347 0    347 347  6.5439118
+# 5  P05    5    a 328   0          0   0 0      0 328 -6.4876840
+# 6  P05    6    d 101 223          0   0 0      0 324 -6.4754327
+
+# calculate ALL column
+data2$all <-  (data2$BackGround
+              + data2$A
+              + data2$B
+              + data2$C
+              + data2$D)
+
+data2$logit <- log((data2$Target + 0.5) / (data2$all - data2$Target + 0.5))
+
+# making 2 by 2
+data2$npi <- ifelse(data2$cond == "a" | data2$cond == "c", 1, 0)
+data2$aff <- ifelse(data2$cond == "a" | data2$cond == "b", 1, 0)
+
+# scaling
+data2$npi <- scale(data2$npi, scale=T)
+data2$aff <- scale(data2$aff, scale=T)
+
+# data2$logit<-data2$logit_c
+tapply(data2$logit, list(data2$npi, data2$aff), mean)
+
+sum(data2[data2$cond == "a",]$Target)
+sum(data2[data2$cond == "b",]$Target)
+
+if(!require(knitr)){install.packages("knitr")}
+if(!require(lmerTest)){install.packages("lmerTest")}
+if(!require(devtools)){install.packages("devtools")}
+install_github("kisiyama/mudball",ref="master",force=TRUE)
+require(mudball)
+
+model <- lmer(logit ~ npi * aff + (1 + npi*aff |subj) + (1 + npi*aff |item), data = data2)
+model_0 <- lmer(logit ~ npi * aff + (1 |subj) + (1 |item), data = data2)
+
+best_model = mudball::step(model,beeping=T)
+best_model_summary = lmerTest::summary(best_model)
+fixed_effects = best_model_summary$coefficients
+model_call = best_model_summary$call
+log_name = "lmer.log"
+write("#############################",file=log_name, append=TRUE)
+write("",file=log_name, append=TRUE)
+kable(fixed_effects, format = "markdown")
+write("",file=log_name, append=TRUE)
+write(as.character(model_call), file=log_name, append=TRUE)
+write("#############################", file=log_name, append=TRUE)
+
+m00wi  <-  lmer(logit ~ npi * aff + (1|subj) + (1|item), data = data2)
+m00woi  <-  lmer(logit ~ npi + aff + (1|subj) + (1|item), data = data2)
+m00won  <-  lmer(logit ~ aff + npi:aff + (1|subj) + (1|item), data = data2)
+m00woa  <-  lmer(logit ~ npi + npi:aff + (1|subj) + (1|item), data = data2)
+
+summary(m00wi)
+
+# npi
+anova(m00wi, m00won)
+# aff
+anova(m00wi, m00woa)
+# npi:aff(interaction)
+anova(m00wi, m00woi)
+```
---
+++ script/data-visualizing.r
@@ -0,0 +1,172 @@
+# data visualizing
+
+we are going to see the eye-movements of participants
+toward the target entity.
+
+1. set up (set wd, import libs, and read data)
+
+```R
+getwd()
+setwd("/home/kishiyama/home/thesis/ntu-ut-ling-vwp/result")
+# kishiyama for linux, kisiyama for windows
+setwd("/home/kisiyama/home/thesis/ntu-ut-ling-vwp/result")
+
+library(ggplot2)
+library(reshape)
+if(!require(lme4)){install.packages("lme4")}
+if(!require(reshape)){install.packages("reshape")}
+if(!require(reshape2)){install.packages("reshape2")}
+if(!require(ggplot2)){install.packages("ggplot2")}
+if(!require(knitr)){install.packages("knitr")}
+if(!require(beepr)){install.packages("beepr")}
+if(!require(lmerTest)){install.packages("lmerTest")}
+
+data_all <- read.csv("./output.csv", header =T)
+summary(data_all)
+head(data_all)
+```
+
+1. digitalize the data
+
+```R
+# define the span for the graph here
+span_begin <- 4100
+span_end <- 5500
+
+# Generating a sequence from span_begin to span_end by 20 ms.
+pol_n2 <- seq(from=span_begin,to=span_end,20)
+
+# make a table for binary data 
+# nrow(data_all) == 20621
+binary_data <- matrix(span_begin, nrow=nrow(data_all), ncol=length(pol_n2))
+colnames(binary_data) <- pol_n2
+
+# from span begin to end, check if there is gaze event
+for (i in 1:length(pol_n2)){
+    binary_data[,i] <- ifelse(
+        (data_all$GazeStart < (span_begin + i * 20) &
+        data_all$GazeEnd > (span_begin + i * 20)),
+        1,
+        0)}
+
+# combine the binary data with all data
+gr <- cbind(data_all, as.data.frame(binary_data))　
+
+# AOIs has numbers . see the content in the AOI.
+gr$Target <- ifelse(gr$AOI == 1, as.character(gr$AOI1), "BackGround")
+gr$Target <- ifelse(gr$AOI == 2, as.character(gr$AOI2), gr$Target)
+gr$Target <- ifelse(gr$AOI == 3, as.character(gr$AOI3), gr$Target)
+gr$Target <- ifelse(gr$AOI == 4, as.character(gr$AOI4), gr$Target)
+
+# participant,item, cond, AOI,Target,bin
+gr<- gr[,c(1, 9, 10, 15, ncol(gr), 16:(ncol(gr)-1))]
+head(gr)
+
+# melt time binaries into one column.
+gr2 <- melt(gr,id=c("ParticipantName", "ItemNo", "Condition", "AOI", "Target"))
+gr2$variable <- as.numeric(as.character(gr2$variable))
+gr2 <- gr2[order(gr2$ParticipantName, gr2$ItemNo),]
+
+# in the recording of Tobii, count 1 if the AOI is seen in the 20m
+# this causes many duplicates bacause of counting 0 for other AOIs.
+# aggregate remove dups so that no dup in a bin
+gr3 <-aggregate(
+    gr2$value,
+    by = list(gr2$ParticipantName, gr2$ItemNo, gr2$Condition,  gr2$AOI, gr2$Target, gr2$variable),
+    FUN = sum,
+    na.rm = TRUE)
+colnames(gr3) = c("subj","item","cond", "AOI", "variable", "bin","value")
+gr3$AOI <- NULL
+head(gr3)
+
+# gr3 has 2 columns: `variable` and `value`
+# function `cast` makes
+# 1. new cols based on levels in `variable` 
+# 1. new rows based on levels in `value`
+gr.temp <- cast(gr3)
+head(gr.temp)
+
+# N1_V1_t :A
+# N1_V1_f :B
+# N2_V1_t :C
+# N2_V1_f :D
+gr.temp$A <- ifelse(is.na(gr.temp$A),0,gr.temp$A)
+gr.temp$B <- ifelse(is.na(gr.temp$B),0,gr.temp$B)
+gr.temp$C <- ifelse(is.na(gr.temp$C),0,gr.temp$C)
+gr.temp$D <- ifelse(is.na(gr.temp$D),0,gr.temp$D)
+
+# If you need regard two(or more) area as one area,
+# you might want to make some changes here.
+# gr.temp$Combined <- gr.temp$TargetCompound + gr.temp$CompetitorCompound
+# gr.temp$IrrelevantCompound <- gr.temp$IrrelevantCompoundA + gr.temp$IrrelevantCompoundB
+
+# (t1) N1_V1_t (t2) N1_V1_f (t3) N2_V1_t (t4) N2_V1_f
+gr.temp$t1 <- gr.temp$A
+gr.temp$t2 <- gr.temp$B
+gr.temp$t3 <- gr.temp$C
+gr.temp$t4 <- gr.temp$D
+
+# c: correct
+# w: wrong
+gr.temp$c <- ifelse((gr.temp$cond == "a" | gr.temp$cond == "b"), gr.temp$C, gr.temp$D)
+gr.temp$w <- ifelse((gr.temp$cond == "a" | gr.temp$cond == "b"), gr.temp$D, gr.temp$C)
+
+gr.temp$cn1 <- ifelse((gr.temp$cond == "a" | gr.temp$cond == "b"), gr.temp$A, gr.temp$B)
+gr.temp$wn1 <- ifelse((gr.temp$cond == "a" | gr.temp$cond == "b"), gr.temp$B, gr.temp$A)
+
+#aggregate for graph (Use t1~t4)
+gra <- aggregate(
+    gr.temp$c,
+    by=list(gr.temp$bin, gr.temp$cond),
+    mean)
+colnames(gra) <- c("bin", "cond", "mean")
+
+# a: NPI+AFF
+# gra$cond <- ifelse((gra$cond == 1), "NPI_t_AFF_t", gra$cond)
+# gra$cond <- ifelse((gra$cond == 2), "NPI_f_AFF_t", gra$cond)
+# gra$cond <- ifelse((gra$cond == 3), "NPI_t_AFF_f", gra$cond)
+# gra$cond <- ifelse((gra$cond == 4), "NPI_f_AFF_f", gra$cond)
+
+# rename the cond name
+library(ggplot2)
+library(reshape)
+library(plyr)
+gra$cond=
+    revalue(gra$cond,
+        c("a"="NPI+AFF+:(sika->aru)",
+        "b"="NPI-AFF+:(dake->aru)",
+        "c"="NPI+AFF-:(sika->nai)",
+        "d"="NPI-AFF-:(dake->nai)")
+    )
+
+# make a graph
+d = data.frame(t = c(4100, 4700 , 5500),
+          region = c("V1", "POL", "N2"))
+g.Combined.Region6000 <-
+    ggplot(data=gra, aes(x=bin, y=mean, colour = cond))+
+    # we can draw some vertical lines
+    geom_vline(data=d, mapping=aes(xintercept=t), linetype=3, color="black") +
+    geom_text(data=d, mapping=aes(x=t, y=0.45, label=region),
+        size=5, angle=90, vjust=-0.4, hjust=0 , color="#222222", family="mono") +
+    # geom_line(aes(group=cond, color=cond, alpha = 0.99)) +
+    geom_line(aes(group=cond, color=cond)) +
+    # geom_point(aes(group=cond, color=cond)) +
+    scale_x_continuous("Time") +
+    scale_y_continuous(limits=c(0,0.5), name="Proportion of looks to target N2") +
+    scale_color_discrete("Condition") +
+    theme(axis.title.y = element_text(size = 16)) +
+    theme(axis.title.x = element_text(size = 20)) +
+    theme(legend.title = element_text(size = 16)) +
+    theme(legend.text = element_text(size = 20))+
+    # theme_bw() +
+    theme_classic()+
+    ggtitle("Proportion of gaze to N2 doing V1"
+)  
+
+ppi <- 600
+g.Combined.Region6000
+# png("../png/n2_target_full.png", width=12*ppi, height=6*ppi, res=ppi)
+dev.copy(pdf, "../pdf/n2_v1_full.pdf")
+
+dev.off()
+```
